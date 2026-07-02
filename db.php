d779c17c6da07e8216ba7941467c6b6e90d2d4ed
bonkval <cedrickvales1111@gmail.com>
2026-07-03 01:23:44 +0800

    Update database connection credentials and name

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "adidas_store";
+$host = "sql300.infinityfree.com"; 
+$user = "if0_42322855";        
+$pass = "06591507851aA"; 
+$db_name = "if0_42322855_adidas_store"; 
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $pass, $db_name);
 
 if (!$conn) {
-    die("Database connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
 ?>
